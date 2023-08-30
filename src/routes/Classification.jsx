6d54792345a6f460fef740f5d539827e9ac2e187
tharindu_107365 <tharindu.madhushan@axiatadigitallabs.com>
2023-08-30 23:24:54 +0530

    api

--- src/routes/Classification.jsx
+++ src/routes/Classification.jsx
@@ -1,9 +1,7 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus, faStarHalfStroke } from "@fortawesome/free-solid-svg-icons";
 import SidePanel from "../components/SidePanel";
-//identification
+
 function Classification() {
   const [selectedImage, setSelectedImage] = useState(null);
   const [respo, setResponse] = useState(null);
@@ -31,7 +29,6 @@ function Classification() {
       console.log(error);
     }
   };
-
   const getHighestCategory = () => {
     let highestPercentage = 0;
     let highestCategory = "";
@@ -45,65 +42,65 @@ function Classification() {
         highestCategory = category;
       }
     }
-
+    setRespClassification(highestCategory);
     return highestCategory;
   };
-  <div className="main-body w-full bg-slate-100 h-screen">
-    <div className="main-body-container h-full w-full flex flex-row">
-      <div>
-        <SidePanel />
-      </div>
+  return (
+    <div className="main-body w-full bg-slate-100 h-screen">
+      <div className="main-body-container h-full w-full flex flex-row">
+        <div>
+          <SidePanel />
+        </div>
 
-      <div className="w-full justify-center items-center flex">
-        <div className="bg-white mx-auto p-10 w-[80%] flex flex-col justify-center overflow-clip shadow-md shadow-slate-300 rounded-md">
-          <h3 className="mb-3 text-center text-xl z-10">
-            Brain Tumor calssification
-          </h3>
+        <div className="w-full justify-center items-center flex">
+          <div className="bg-white mx-auto p-10 w-[80%] flex flex-col justify-center overflow-clip shadow-md shadow-slate-300 rounded-md">
+            <h3 className="mb-3 text-center text-xl z-10">
+              Brain Tumor classification
+            </h3>
 
-          <div className="dash-body flex flex-row w-full p-3 justify-between space-x-3 items-center">
-            <div className="upload-image text-center p-2 w-full">
-              <div className="upload border border-gray-300 p-2 rounded-md w-full">
-                <input
-                  type="file"
-                  accept="image/*"
-                  onChange={handleImageChange}
-                  className="hidden"
-                  id="image-upload"
-                />
-                <label
-                  htmlFor="image-upload"
-                  className="block cursor-pointer text-[32pt]"
-                >
-                  <span className="sr-only">Upload an Image</span>
-                </label>
-                {selectedImage && (
-                  <img
-                    src={URL.createObjectURL(selectedImage)}
-                    alt="Selected"
-                    className=" mx-auto w-full h-full object-cover rounded-md"
+            <div className="dash-body flex flex-row w-full p-3 justify-between space-x-3 items-center">
+              <div className="upload-image text-center p-2 w-full">
+                <div className="upload border border-gray-300 p-2 rounded-md w-full">
+                  <input
+                    type="file"
+                    accept="image/*"
+                    onChange={handleImageChange}
+                    className="hidden"
+                    id="image-upload"
                   />
-                )}
+                  <label
+                    htmlFor="image-upload"
+                    className="block cursor-pointer text-[32pt]"
+                  >
+                    <span className="sr-only">Upload an Image</span>
+                  </label>
+                  {selectedImage && (
+                    <img
+                      src={URL.createObjectURL(selectedImage)}
+                      alt="Selected"
+                      className=" mx-auto w-full h-full object-cover rounded-md"
+                    />
+                  )}
+                </div>
+
+                <div className="mt-5">
+                  <label
+                    htmlFor="image-upload"
+                    className="mt-3 bg-purple-300 p-2 w-[130px] rounded-full text-white font-semibold cursor-pointer"
+                  >
+                    Upload an Image
+                  </label>
+                </div>
+                <div className="mt-5">
+                  <button
+                    onClick={handleSubmit}
+                    className="p-3 bg-sky-300 rounded-full w-[120px] text-white"
+                  >
+                    Predict
+                  </button>
+                </div>
               </div>
 
-              <div className="mt-5">
-                <label
-                  htmlFor="image-upload"
-                  className="mt-3 bg-purple-300 p-2 w-[130px] rounded-full text-white font-semibold cursor-pointer"
-                >
-                  Upload an Image
-                </label>
-              </div>
-              <div className="mt-5">
-                <button
-                  onClick={() => handleSubmit()}
-                  className="p-3 bg-sky-300 rounded-full w-[120px] text-white"
-                >
-                  Predict
-                </button>
-              </div>
-            </div>
-
-            {respo && (
               <div className="pre-process text-center p-2 w-full mb-5">
                 <div className="p-5 mx-auto mt-10 flex flex-col w-full space-y-5 text-center justify-center ">
                   <div className="res w-full flex flex-row space-x-5 justify-between">
@@ -136,12 +133,12 @@ function Classification() {
                   </div>
                 </div>
               </div>
-            )}
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>;
+  );
 }
 
 export default Classification;
